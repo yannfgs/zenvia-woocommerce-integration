54a7ea1e9065a2942c08a8cc980517cdf452f401
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2024-09-10 17:32:55 -0300

    Update zenvia-woocommerce-integration.php

--- zenvia-woocommerce-integration/zenvia-woocommerce-integration.php
+++ zenvia-woocommerce-integration/zenvia-woocommerce-integration.php
@@ -17,7 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-zenvia-api.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-zenvia-woocommerce.php';
 
-
 // Inicializa o plugin
 function zenvia_woocommerce_integration_init() {
     Zenvia_WooCommerce::init();
